7c0302e222ac173f740bf6f3ae4809896e896b6e
Rida Mumtaz <mumtazrida6@gmail.com>
2021-09-17 11:22:13 +0530

    First Check

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-//jshint esversion:6
+
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require("mongoose");
-const _ = require("lodash");
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Welcome to my BlogPost Application! Fancy seeing yourself here. I'm currently working to deploy this, learning and constantly adding new features in it until it all comes together to be what I've envisioned it to be. Try running this application once again by forking the updated code after sometime.";
 const aboutContent = "Welcome to my BlogPost Application! Fancy seeing yourself here. I'm currently working to deploy this, learning and constantly adding new features in it until it all comes together to be what I've envisioned it to be. Try running this application once again by forking the updated code after sometime.";
 const contactContent = "Welcome to my BlogPost Application! Fancy seeing yourself here. I'm currently working to deploy this, learning and constantly adding new features in it until it all comes together to be what I've envisioned it to be. Try running this application once again by forking the updated code after sometime.";
-
 
 const app = express();
 
@@ -34,39 +33,35 @@ app.get("/", function(req, res){
       posts: posts
       });
   });
-
 });
-
 
 app.get("/compose", function(req, res){
   res.render("compose");
 });
 
 app.post("/compose", function(req, res){
-
-  const post = new Post ({
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
   });
 
-  post.save();
 
-  res.redirect("/");
-
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
 });
 
-app.get("/posts/:postName", function(req, res){
-  const requestedTitle = _.lowerCase(req.params.postName);
+app.get("/posts/:postId", function(req, res){
 
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
+const requestedPostId = req.params.postId;
 
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-    }
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
   });
 
 });
@@ -78,6 +73,7 @@ app.get("/about", function(req, res){
 app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
